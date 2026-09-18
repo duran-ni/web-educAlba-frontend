895b42b6308d5a9f54ec6615189cc983dfa52159
duran-ni <duran_ni_7@hotmail.com>
2026-09-18 15:14:14 +0200

    feat: render footer in the app layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import AppHeader from '@/components/AppHeader.vue'
   <main class="app">
     <RouterView />
   </main>
+  <AppFooter />
 </template>
 
 <style lang="scss">
