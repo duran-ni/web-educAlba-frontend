32814e0964236c8c2ce61cd32a7ea5441cb9e8ac
duran-ni <duran_ni_7@hotmail.com>
2026-09-18 15:14:01 +0200

    feat: compose footer blocks into AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="app-footer">
+    <FooterContact />
+    <FooterSocial />
+    <FooterLegal />
+  </footer>
+</template>
+
+<script setup>
+import FooterContact from './FooterContact.vue'
+import FooterSocial from './FooterSocial.vue'
+import FooterLegal from './FooterLegal.vue'
+</script>
+
+<style lang="scss">
+.app-footer {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  padding: 2rem 1.5rem;
+  background-color: $color-background-alt;
+
+  @include respond-to(tablet) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+}
+</style>
