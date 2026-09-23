dea2cad7f1aee25cc2021358ece2df6aa960c163
duran-ni <duran_ni_7@hotmail.com>
2026-09-23 15:14:12 +0200

    feat: show the hero welcome message on the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <PageHeading title="Inicio" />
+  <HeroWelcome />
 </template>
 
 <script setup>
 import PageHeading from '@/components/PageHeading.vue'
+import HeroWelcome from '@/components/HeroWelcome.vue';
 </script>
