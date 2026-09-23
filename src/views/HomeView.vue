4d4a7a31ba877bb7083f4cdd69f6e4479cf2e065
duran-ni <duran_ni_7@hotmail.com>
2026-09-23 20:21:23 +0200

    feat: show the quick contact CTA on the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <PageHeading title="Inicio" />
   <HeroWelcome />
+  <QuickContactCta />
 </template>
 
 <script setup>
 import PageHeading from '@/components/PageHeading.vue'
 import HeroWelcome from '@/components/HeroWelcome.vue';
+import QuickContactCta from '.@/components/QuickContactCta.vue';
 </script>
