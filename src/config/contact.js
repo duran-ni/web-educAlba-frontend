819493052250a708ceaab112593a94a66bdf9d8c
duran-ni <duran_ni_7@hotmail.com>
2026-09-23 20:10:51 +0200

    feat: centralize contact data in a shared config file

---
+++ src/config/contact.js
@@ -0,0 +1,8 @@
+// Datos de contacto de la academia, centralizados para no duplicarlos en cada componente/vista
+export const CONTACT = {
+  address: 'C/ Cooperativa Pablo Iglesias, 2 bajo 10 33920 Riaño-Langreo',
+  phone: '+34 622 29 13 09',
+  phoneHref: 'tel:+34622291309',
+  email: 'refuerzoeducalba@gmail.com',
+  emailHref: 'mailto:refuerzoeducalba@gmail.com',
+}
