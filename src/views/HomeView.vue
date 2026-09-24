c0c5469fac2b912fe39374bcfbc2ba719b02ebe0
duran-ni <duran_ni_7@hotmail.com>
2026-09-24 11:33:58 +0200

    feat: add quick interest form to the Home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <HeroWelcome />
+  <QuickInterestForm />
   <QuickContactCta />
 </template>
 
 <script setup>
 import HeroWelcome from '@/components/HeroWelcome.vue';
+import QuickInterestForm from '@/components/QuickInterestForm.vue'
 import QuickContactCta from '@/components/QuickContactCta.vue';
 </script>
