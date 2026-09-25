26d482c14bff74ee7c59b1942e5cd0d9c08a2d30
duran-ni <duran_ni_7@hotmail.com>
2026-09-25 17:30:34 +0200

    feat: connect the Workshops view to the workshops list and restyle its heading

--- src/views/WorkshopsView.vue
+++ src/views/WorkshopsView.vue
@@ -1,7 +1,49 @@
 <template>
-  <PageHeading title="Talleres" />
+  <header class="workshops-heading">
+    <h1 class="workshops-heading__title">
+      Talleres
+      <span class="workshops-heading__highlight" aria-hidden="true"></span>
+    </h1>
+  </header>
+
+  <WorkshopsList />
 </template>
 
 <script setup>
-import PageHeading from '@/components/PageHeading.vue'
+import WorkshopsList from '@/components/WorkshopsList.vue'
 </script>
+
+<style lang="scss">
+.workshops-heading {
+  text-align: center;
+  margin-top: 2rem;
+  margin-bottom: 2.5rem;
+
+
+  &__title {
+    position: relative;
+    display: inline-block;
+    margin: 0;
+    font-family: $font-doodle;
+    font-size: 3.5rem;
+    color: $color-primary;
+
+    @include respond-to(tablet) {
+      margin-top: 3rem;
+      font-size: 4rem;
+    }
+  }
+
+  &__highlight {
+    position: absolute;
+    left: 0;
+    bottom: -0.25rem;
+    width: 100%;
+    height: 0.5rem;
+    background-color: $color-accent-yellow;
+    opacity: 0.5;
+    transform: rotate(-2deg);
+    z-index: -1;
+  }
+}
+</style>
